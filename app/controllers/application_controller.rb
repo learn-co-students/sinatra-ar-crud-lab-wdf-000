bf6ed9cb6d2e66d767959869e3c82d11a20e7f24
fidelscodes <fidelscodes@gmail.com>
2016-08-11 17:16:15 -0400

    Create Action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,17 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  # get '/' do
+  #
+  # end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    # binding.pry
+    erb :posts
   end
 end
