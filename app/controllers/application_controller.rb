c87729e9479abb3e4b7ce23f8a5dd67f10ded316
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-08-25 13:55:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,84 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  # configure do
+  #   set :public_folder, 'public'
+  #   set :views, 'app/views'
+  # end
+  #
+  # get '/posts/new' do
+  #   erb :new
+  # end
+  #
+  # post '/posts' do
+  #  @post =  Post.create(params)
+  #  redirect to '/posts'
+  # end
+  #
+  # get '/posts' do
+  #    @posts = Post.all
+  #    erb :index
+  # end
+  #
+  # get '/posts/:id' do
+  #    @post = Post.find_by_id(params[:id])
+  #    erb :show
+  # end
+  #
+  # get '/posts/:id/edit' do
+  #   @post = Post.find_by_id(params[:id])
+  #   erb :edit
+  # end
+  #
+  # post 'posts/:id' do
+  #   @post = Post.find_by_id(params[:id])
+  #  @post.name = params[:name]
+  #  @post.content = params[:content]
+  #  @post.save
+  #  erb :show
+  # end
+
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+  set :public_folder, 'public'
+  set :views, 'app/views'
+end
+
+get '/posts/new' do #loads new form
+  erb :new
+end
+
+post '/posts' do  #creates a post
+  @post = Post.create(params)
+  redirect to '/posts'
+end
+
+get '/posts' do #loads index page
+  @posts = Post.all
+  erb :index
+end
+
+get '/posts/:id' do  #loads show page
+  @post = Post.find_by_id(params[:id])
+  erb :show
+end
+
+get '/posts/:id/edit' do #loads edit form
+  @post = Post.find_by_id(params[:id])
+  erb :edit
+end
+
+post '/posts/:id' do  #updates a post
+  @post = Post.find_by_id(params[:id])
+  @post.name = params[:name]
+  @post.content = params[:content]
+  @post.save
+  erb :show
+end
+
+post '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
   end
 
-  get '/' do 
-    
-  end
 end
