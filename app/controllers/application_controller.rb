623f7bf6efeafc13fadf05af7a7d3ad267bfcade
Kelene Chung <simoy93@gmail.com>
2016-09-09 12:17:50 -0400

    Sinatra ar crud lab DONE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,4 +11,44 @@ class ApplicationController < Sinatra::Base
   get '/' do 
     
   end
+
+  get '/posts/new' do 
+  	erb :new
+  end
+
+  post '/posts' do 
+  	@post = Post.create(params)
+  	redirect '/posts'
+  end
+
+  get '/posts' do
+  	@posts = Post.all
+    # binding.pry 
+  erb :index 
+  end
+
+  get '/posts/:id' do 
+    @post = Post.find(params[:id])
+    # binding.pry
+    erb :show
+  end
+
+  get '/posts/:id/edit' do 
+    @post = Post.find(params[:id])
+    # binding.pry
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(:name => params[:name], :content => params[:content])
+    redirect "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id/delete' do 
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :deleted
+  end
+
 end
